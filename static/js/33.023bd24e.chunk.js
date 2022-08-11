b60473c9551657952d218db7677dc468c600289a
Andrea Mancini <mancioman2@gmail.com>
2022-08-11 11:16:16 +0200

    first commit

---
+++ static/js/33.023bd24e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmanciotechweb=this.webpackJsonpmanciotechweb||[]).push([[33],{213:function(e,c,t){"use strict";t.r(c),t.d(c,"default",(function(){return u}));var n=t(0),i=t(2),s=t(1);function u(){var e=Object(i.f)();return Object(n.useEffect)((function(){setTimeout((function(){e("/menu")}),2e3)}),[e]),Object(s.jsxs)("div",{children:[Object(s.jsx)("p",{children:"404 - NOT FOUND"}),Object(s.jsx)("p",{children:"Please check the URL"})]})}}}]);
+//# sourceMappingURL=33.023bd24e.chunk.js.map
